716ecc51512b39d49542c899c4a5abcc3dd5453f
rump <git@seandunaway.com>
2013-02-05 19:12:41 -0800

    increase timeout and set content type

--- routes/webcal.js
+++ routes/webcal.js
@@ -18,8 +18,8 @@ app.get('/webcal.ics', function (req, res, next) {
       publix.parse(agent.text, function (err, events) {
         if (err) return next(err);
 
-        // res.setHeader('content-type', 'text/calendar');
-        res.setHeader('content-type', 'text/plain');
+        res.setHeader('content-type', 'text/calendar');
+        // res.setHeader('content-type', 'text/plain');
         return res.render('webcal', { events: events });
       });
     });
