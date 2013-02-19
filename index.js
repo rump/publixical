2f73dfcc1c6a9255fa6ba1bc5b3bf6100a479c5e
rump <git@seandunaway.com>
2013-02-19 11:58:53 -0800

    fix for development

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ for (var config_key in config) {
   app.set(config_key, config[config_key]);
 }
 
-app.use(express.compress());
-app.use(express['static']('./public', { maxAge: 1000 * 60 * 60 * 24 * 30 }));
 if (app.get('env') === 'development') {
   app.use(express.logger());
 }
 
+if (app.get('env') === 'production') {
+  process.title = 'publixical';
+  process.setgid('www-data');
+  process.setuid('nobody');
+}
+
+app.use(express['static']('./public', { maxAge: 1000 * 60 * 60 * 24 * 30 }));
 app.use(app.router);
 fs.readdirSync('./routes').forEach(function (file) {
   require('./routes/' + file);
 });
 
 app.listen(app.get('port'));
-
-process.title = 'publixical';
-process.setgid('www-data');
-process.setuid('nobody');
-
 console.log('OK');
