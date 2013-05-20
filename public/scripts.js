a307d973a7253b044ebdcc1076ba2221d3cae548
rump <git@seandunaway.com>
2013-05-20 14:49:10 -0700

    remove palmer

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,7 @@
 'use strict';
+
+(function () {
+  var m = "\x70\x75\x62\x6C\x69\x78\x69\x63\x61\x6C\x40\x73\x65\x61\x6E\x64\x75\x6E\x61\x77\x61\x79\x2E\x63\x6F\x6D";
+  document.getElementById('contact').href = 'mailto:' + m + '?Subject=Publix%20iCal';
+  document.getElementById('contact').innerHTML = m;
+}());
