fa7af1b49d4124c19ebb98386422a8d470eb18fd
rump <git@seandunaway.com>
2013-05-27 10:14:33 -0700

    add please wait after submit

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,23 @@
 'use strict';
 
 (function () {
+  var s = document.getElementById('subscribe');
+  if (s) {
+    var v = s.value;
+    s.form.onsubmit = function (e) {
+      e.preventDefault();
+      s.value = 'Please wait...';
+      setTimeout(function () {
+        s.form.submit();
+      }, 0);
+      setTimeout(function () {
+        s.value = v;
+      }, 1000 * 60);
+    };
+  }
+}());
+
+(function () {
   var c = document.getElementById('contact');
   if (c) {
     var m = "\x70\x75\x62\x6C\x69\x78\x69\x63\x61\x6C\x40\x73\x65\x61\x6E\x64\x75\x6E\x61\x77\x61\x79\x2E\x63\x6F\x6D";
