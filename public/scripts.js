29cd6c96c34d21d8385a559b92284aba7e406e25
rump <git@seandunaway.com>
2013-05-27 00:53:53 -0700

    add hidden style

--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,6 @@
 'use strict';
 
 (function () {
-  var s = document.getElementById('subscribe');
-  if (s) {
-    s.onclick = function () {
-      setTimeout(function () {
-        var t = document.getElementById('throbber');
-        t.removeClass('hidden');
-        setTimeout(function () {
-          t.addClass('hidden');
-        }, 60 * 1000);
-      }, 1 * 1000);
-    };
-  }
-}());
-
-(function () {
   var c = document.getElementById('contact');
   if (c) {
     var m = "\x70\x75\x62\x6C\x69\x78\x69\x63\x61\x6C\x40\x73\x65\x61\x6E\x64\x75\x6E\x61\x77\x61\x79\x2E\x63\x6F\x6D";
@@ -30,7 +15,7 @@
     var s = document.createElement('script');
     s.src = '/jstz.min.js';
     s.onload = function () {
-      u.innerHTML = u.innerHTML + '&tz=' + jstz.determine().name();
+      u.innerHTML = u.innerHTML + '&tz=' + window.jstz.determine().name();
     };
     document.getElementsByTagName('body')[0].appendChild(s);
   }
