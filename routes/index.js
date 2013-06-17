9a969bf86d452c2d257bcd1d0e37be79ecc6d328
rump <git@seandunaway.com>
2013-06-17 11:32:41 -0700

    kill the site per palmers request

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', express.bodyParser(), function (req, res, next) {
+  // disable
+  return next(new Error('403:Forbidden. This site is now offline.'));
+
   var host = req.headers.host || app.settings.host + ":" + app.settings.port;
   var publix = require('../lib/publix');
   var secret = require('../lib/secret').cipher;
